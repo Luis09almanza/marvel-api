86dec40c208ec94f6a623e0ceb66a1e99ee28877
Salvador Perez <a6520150017@utch.edu.mx>
2022-03-06 13:09:46 -0700

    I add the description to the characters in the main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
 import { useState, useEffect } from 'react';
-import axios, { Axios } from 'axios';
+import  axios from 'axios';
+import { Link } from 'react-router-dom';
+var randomNum = Math.floor(Math.random() * 20); 
+
 //https://gateway.marvel.com:443/v1/public/characters?apikey=84c9afc670217a09f2566ca3189177f9
 
 //key publica: 84c9afc670217a09f2566ca3189177f9
@@ -11,6 +14,10 @@ import axios, { Axios } from 'axios';
 //19a4f1874e8f4c201b8895f1aaf372fdf3fa614a184c9afc670217a09f2566ca3189177f9
 
 //hash: 273ddb932eb9647e18e21c589d55f000
+//<img
+//className="Character__image" 
+//src={`${per.thumbnail.path}.${per.thumbnail.extension}`}
+//>
 
 //https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=84c9afc670217a09f2566ca3189177f9&hash=273ddb932eb9647e18e21c589d55f000
 
@@ -18,14 +25,13 @@ import axios, { Axios } from 'axios';
 
 function App() {
 
-  
   //esta es una variable para guardar los personajes
   const [characters, setCharacters]=useState([])
 
 
   useEffect(()=>{
     //Usando axios obtenemos la informacion del link de marvel
-    axios.get('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=84c9afc670217a09f2566ca3189177f9&hash=273ddb932eb9647e18e21c589d55f000').then(res=>{
+    axios.get('https://gateway.marvel.com:443/v1/public/characters?series=19&ts=1&apikey=84c9afc670217a09f2566ca3189177f9&hash=273ddb932eb9647e18e21c589d55f000').then(res=>{
         setCharacters(res.data.data.results)
         
     }).catch(error=>console.log(error))
@@ -34,7 +40,8 @@ function App() {
 
   console.log(characters)
   return (
-    <div className='App'>
+    <React.Fragment>
+      <div className='App'>
       <h1>Marvel Api using React</h1>
 
         <div className='Characters_data'>
@@ -42,21 +49,26 @@ function App() {
 
             <div className="Character__container">
                 <img
-                    className="Character__image"
+                    className="Character__image" 
                     src={`${per.thumbnail.path}.${per.thumbnail.extension}`}
                 />
             <div className="Character__data">
-                <h3 className="Character__name">{per.name}</h3>
+                <h1 className="Character__name">Name: {per.name}</h1>
+                <h3 className="Character__urls">Comics available: {per.comics.available}</h3>
+                <h3 className="Character__urls">Series available: {per.series.available}</h3>
+                <h4 className="Character__modified">Last Modification: {per.modified}</h4>
+                <p className="Character__description">{per.description || "No description"}</p>
+
+
             </div>
         </div>
 
-          ))
-
-          }
-
+          ))}
+  
 
         </div>
     </div>
+    </React.Fragment>
   );
 }
 
